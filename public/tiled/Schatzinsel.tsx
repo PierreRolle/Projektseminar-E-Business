9305d11a70c07df4046a27160d6d9876d40d88f6
PierreRolle <117457431+PierreRolle@users.noreply.github.com>
2022-11-16 22:28:15 +0100

    Add files via upload

---
+++ public/tiled/Schatzinsel.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Schatzinsel" tilewidth="80" tileheight="80" tilecount="70" columns="10">
+ <image source="Schatzinsel.png" width="800" height="600"/>
+</tileset>
