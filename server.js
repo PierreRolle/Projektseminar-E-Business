a2c87c9f44b94e01a816386b10a385073749e3b4
Ludwig Fritsch <ludwig-fritsch@outlook.de>
2022-12-13 14:41:11 +0100

    Log port on server start

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const path = require("path");
 
 const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  "/static",
+  express.static(path.resolve(__dirname, "frontend", "static"))
+);
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+  res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
