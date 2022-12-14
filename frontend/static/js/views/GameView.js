84b8e2a03b06e8d6f71a2d3159715bfcf8f90835
Ludwig Fritsch <ludwig-fritsch@outlook.de>
2022-12-14 22:29:15 +0100

    Add tnt-count and timer

--- frontend/static/js/views/GameView.js
+++ frontend/static/js/views/GameView.js
@@ -11,6 +11,11 @@ export default class extends AbstractView {
     return `
       <div class="gameContainer">
         <div class="gameHeader">
+          <div style="font-weight:bold; display:flex">
+            <div style="padding-right: 10px">Tnt:</div>
+            <div id="tnt-count">0</div>
+          </div>
+          <div id="timer" style="font-weight:bold">90 Sek.</div>
           <a href="/mainMenu" class="closeButton" data-link>X</a> 
         </div>
         <div>
