129613bee6fafea19359fcd1a9f03ee73204d94b
PierreRolle <pierre.rolle@student.uni-halle.de>
2023-01-04 10:36:27 +0100

    Endscreen css't

--- frontend/static/js/views/EndView.js
+++ frontend/static/js/views/EndView.js
@@ -8,12 +8,12 @@ export default class extends AbstractView {
 
   async getHtml() {
     return `
-    <div class="manualContainer">
+    <div class="manualContainerEnd">
         <div class="manualButtonContainer">
             <a href="/mainMenu" class="closeButton" data-link>X</a>
         </div>
-        <img src="/static/images/Schatzinsel_Überschrift.png" width = "500px">
-        <img src="/static/images/Schatz_geborgen.png" width = "500px">
+        <img class="headerEnd imageEnd" src="/static/images/Schatzinsel_Überschrift.png" width = "500px">
+        <img class="imageEnd" src="/static/images/Schatz_geborgen.png" width = "500px">
         <img src="/static/images/Herzlichen_Glückwunsch.png" width = "500px">
         <a href="/mainMenu" class="button" data-link>Zurück zum Hauptmenü!</a>
     </div>
