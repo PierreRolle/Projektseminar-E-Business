86dadb36130088e5fc9eff30f5861ea00e841242
L0rinator <EnricoRie@gmx.net>
2023-01-05 23:40:44 +0100

    Added 2nd Manualpage

---
+++ frontend/static/js/views/ManualView2.js
@@ -0,0 +1,77 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView {
+  constructor(probs) {
+    super(probs);
+    this.setTitle("Handbuch_2");
+  }
+
+  async getHtml() {
+    return `
+        <div class="manualContainer">
+          <div class="manualButtonContainer">
+            <a href="/manual" class="closeButton" data-link> <img src="/static/images/tiles/Zurueck.png"
+            height = "24px" width = "24px" > </a>
+            <a href="/mainMenu" class="closeButton" data-link><img src="/static/images/Exit.png"
+            height = "24px" width = "24px" ></a>
+          </div>
+          <h2 class="header">Dein Freund und Helfer</h2>
+          <p>
+          <div class="Gegner">
+            Gegner:</br></br>
+          </div>
+
+          <div class="ItemsandEnemies"> 
+            <img src="/static/images/tiles/Fledermaus.png" height = "48px" width = "48px">
+            -  
+            <div class="minus"> Ein vertikal gehender Gegner. Trifft sie auf eine Wand, dreht sie um. 
+            Hüte dich vor ihren fiesen Zähnen.
+            Sie ist außerdem sehr robust und dementsprechend nicht wegsprengbar.
+            </div> 
+          </div>
+          </br>
+
+          <div class="ItemsandEnemies"> 
+            <img src="/static/images/tiles/Käfer.png" height = "48px" width = "48px">
+            -  
+            <div class="minus"> Ein horizontal gehender Gegner. Trifft er auf eine Wand, dreht er um. 
+            Hüte dich vor seinen spitzen Mundwerkzeugen.</br>
+            Er ist sehr robust und dementsprechend nicht wegsprengbar.
+            </div>
+          </div></br>
+          
+
+          <div class="Items">
+            Items: </br></br>
+          </div>
+          <div class="ItemsandEnemies">
+            <img src="/static/images/tiles/Tnt-klein.png" height = "48px" width = "48px">
+            -  
+            <div class="minus">  Deine wichtigsten Freunde auf deiner Reise. 
+            Kann zum Steine sprengen genutzt werden.
+            </div> </br>
+
+          </div>
+          </br>
+
+          <div class="ItemsandEnemies">
+            <img src="/static/images/tiles/Stein.png" height = "48px" width = "48px">
+            -  
+            <div class="minus">  
+            Dein häufigster Feind. Er steht im Weg und muss weg. 
+            Kann mit Dynamit weggesprengt werden. </br>
+            </div>
+          </div>
+          </br>
+          <div class="ItemsandEnemies">
+            <img src="/static/images/tiles/Teleporter.png" height = "48px" width = "48px">
+            -  
+            <div class="minus"> 
+            Ist meist im Duo anzutreffen. Kann benutzt werden zum teleportieren.</br>
+            </div>
+          </div>
+          </p>
+      </div>
+    `;
+  }
+}
